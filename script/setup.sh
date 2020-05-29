b977ba08c7d928ac3a19c9fe18211b6b59c76080
exoego <ytatsuno.jp@gmail.com>
2020-05-29 18:53:52 +0900

    Remove unlink already exists

--- script/setup.sh
+++ script/setup.sh
@@ -4,6 +4,7 @@ SOURCE_DIR=$(pwd)/app/nodejs-v14/src/main
 
 V12_DIR=$(pwd)/app/nodejs-v12/src/main
 if [ -e "$V12_DIR" ]; then
+  unlink "$V12_DIR"
   echo "V12 dir already exists."
 else
   ln -s "$SOURCE_DIR" "$V12_DIR";
@@ -12,6 +13,7 @@ fi
 
 V10_DIR=$(pwd)/app/nodejs-v10/src/main
 if [ -e "$V10_DIR" ]; then
+  unlink "$V12_DIR"
   echo "V10 dir already exists."
 else
   ln -s "$SOURCE_DIR" "$V10_DIR";
