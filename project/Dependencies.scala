088ec07885446684186d42db145ae435579b8c16
Scala Steward <me@scala-steward.org>
2020-06-20 18:19:48 +0200

    Update scalajs-types-util to 0.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val app = Def.setting(
     Seq(
       scalaReflect.value,
-      "net.exoego"               %%% "scalajs-types-util" % "0.1.0",
+      "net.exoego"               %%% "scalajs-types-util" % "0.2.0",
       "org.scalatest"            %%% "scalatest"          % scalatestVersion % "test",
       "com.thoughtworks.enableIf" %% "enableif"           % "1.1.7"
     )
