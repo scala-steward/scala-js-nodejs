031847b4af86e2e9e3060386a46f722af8f87a9c
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-06-07 22:03:15 +0900

    Fix test for Node.js v16

--- app/nodejs-v10/src/test/scala/io/scalajs/nodejs/TestEnvironment.scala
+++ app/nodejs-v10/src/test/scala/io/scalajs/nodejs/TestEnvironment.scala
@@ -9,11 +9,12 @@ object TestEnvironment {
 
   def isWindows: Boolean = os.OS.platform().startsWith("win")
 
+  def isExecutedInExactNode16: Boolean = nodeMajorVersion == 16
   def isExecutedInExactNode14: Boolean = nodeMajorVersion == 14
   def isExecutedInExactNode12: Boolean = nodeMajorVersion == 12
   def isExecutedInExactNode10: Boolean = nodeMajorVersion == 10
-  def isExecutedInExactNode8: Boolean  = nodeMajorVersion == 8
 
+  def isExecutedInNode16OrNewer: Boolean = nodeMajorVersion >= 16
   def isExecutedInNode14OrNewer: Boolean = nodeMajorVersion >= 14
   def isExecutedInNode12OrNewer: Boolean = nodeMajorVersion >= 12
   def isExecutedInNode10OrNewer: Boolean = nodeMajorVersion >= 10
