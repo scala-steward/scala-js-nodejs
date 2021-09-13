0f3ad0cc3feeb7c2cd735a76a4ffb26937a97c4d
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-09-13 22:00:04 +0900

    Annotate

--- app/nodejs-v16/src/main/scala/io/scalajs/nodejs/AbortController.scala
+++ app/nodejs-v16/src/main/scala/io/scalajs/nodejs/AbortController.scala
@@ -1,10 +1,12 @@
 package io.scalajs.nodejs
 
+import com.thoughtworks.enableMembersIf
 import io.scalajs.nodejs.events.EventTarget
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSGlobal
 
+@enableMembersIf(io.scalajs.nodejs.internal.CompilerSwitches.gteNodeJs16)
 @js.native
 @JSGlobal("AbortController")
 class AbortController extends js.Object {
@@ -12,12 +14,14 @@ class AbortController extends js.Object {
   def signal: AbortSignal = js.native
 }
 
+@enableMembersIf(io.scalajs.nodejs.internal.CompilerSwitches.gteNodeJs16)
 @js.native
 @JSGlobal("AbortSignal")
 class AbortSignal private[this] () extends EventTarget {
   def aborted: Boolean = js.native
 }
 
+@enableMembersIf(io.scalajs.nodejs.internal.CompilerSwitches.gteNodeJs16)
 @js.native
 @JSGlobal("AbortSignal")
 object AbortSignal extends js.Object {
